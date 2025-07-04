cfa25f044550856656ae366fdaf14364f5c54622
basileaelterman <basile.aelterman@email.com>
2025-07-04 15:49:07 +0200

    Small optimization

--- even_fibonacci_numbers.js
+++ even_fibonacci_numbers.js
@@ -1,16 +1,16 @@
 let x = 1;
 let y = 2;
 let sum = 0;
-let temp = 0;
 
 while (y <= 4000000) {
     if (y % 2 === 0) {
         sum += y;
     }
 
-    temp = x + y;
+    let temp = x + y;
+
     x = y;
     y = temp;
 }
 
-console.log(sum);
+console.log(sum); // Result: 4613732
